be7eefef83fcc1d0fd4ea6a5795b343b5342c15d
Paras Yadav <62833829+parascoding@users.noreply.github.com>
2022-05-05 14:39:23 +0530

    Create Unique Paths.java

---
+++ Dynamic Programming/Unique Paths.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int uniquePaths(int n, int m) {
+        int dp[]=new int[m];
+        Arrays.fill(dp,1);
+        for(int i=1;i<n;i++)
+            for(int j=1;j<m;j++)
+                dp[j]+=dp[j-1];
+        return dp[m-1];
+    }
+}
